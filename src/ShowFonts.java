1a9a22ce16468d67d370caeae49581c794221bee
sarah du <sdu17@deerfield.edu>
2016-01-27 12:25:00 -0500

    added JD's code to detect all available fonts

--- src/ShowFonts.java
+++ src/ShowFonts.java
@@ -4,10 +4,13 @@ import java.applet.Applet;
 
 public class ShowFonts extends Applet
 {
-
-public ShowFonts() 
-	{
-	
-	}
-
+	   public static void main ( String [] args )
+	      {
+	      GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+	      String[] names = ge.getAvailableFontFamilyNames();
+	      for ( int i=0; i<names.length; i++ )
+	         {
+	             System.out.println( names[i] );
+	         }
+	      }
 }
