da2cde2b2c36db63d745fec8ade5423fe9a10d8e
sarah du <sdu17@deerfield.edu>
2016-01-27 12:39:02 -0500

    added text area, text area can display text and applet will run

--- src/ShowFonts.java
+++ src/ShowFonts.java
@@ -4,13 +4,21 @@ import java.applet.Applet;
 
 public class ShowFonts extends Applet
 {
-	   public static void main ( String [] args )
+	public ShowFonts() 
+	{
+		//create text area
+		JTextArea output = new JTextArea();
+		add(output);
+		output.setText("SAMPLE OUTPUT");
+	}
+		
+	public static void main ( String [] args )
+	{
+      GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+      String[] names = ge.getAvailableFontFamilyNames();
+      for ( int i=0; i<names.length; i++ )
 	      {
-	      GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-	      String[] names = ge.getAvailableFontFamilyNames();
-	      for ( int i=0; i<names.length; i++ )
-	         {
-	             System.out.println( names[i] );
-	         }
-	      }
-}
+	           System.out.println( names[i] );
+	       }	      
+      }
+	}
