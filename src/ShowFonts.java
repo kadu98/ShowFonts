c91e81a56a9668ecd8077ac452e1bd2ea34f5571
sarah du <sdu17@deerfield.edu>
2016-01-27 12:52:11 -0500

    created seperate function to get fonts (with JD's given code), with no input parameter and output string array

--- src/ShowFonts.java
+++ src/ShowFonts.java
@@ -6,6 +6,17 @@ public class ShowFonts extends Applet
 {
 	public ShowFonts() 
 	{
+		//create drop-down menu for font families
+		JComboBox fontFam = new JComboBox();
+		add(fontFam);
+		
+		/*add all fonts to font family drop-down menu
+		for ( int i=0; i<array.length; i++ )
+		{
+			fontFam.addItem( array[i]);
+		}
+		*/
+		
 		//create text area
 		JTextArea output = new JTextArea();
 		add(output);
@@ -14,11 +25,18 @@ public class ShowFonts extends Applet
 		
 	public static void main ( String [] args )
 	{
+	}
+	
+	public static String[] GetFonts()
+	{
       GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
       String[] names = ge.getAvailableFontFamilyNames();
+      int numFonts = names.length;
       for ( int i=0; i<names.length; i++ )
 	      {
 	           System.out.println( names[i] );
 	       }	      
-      }
-	}
+      return names[];
+     }
+	
+}
